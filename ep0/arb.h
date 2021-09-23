83633ea57e99f960cb9ef3394087066aa01a8149
Rafael Badain <badain@usp.br>
2021-09-22 21:44:25 -0300

    dfs boost

--- ep0/arb.h
+++ ep0/arb.h
@@ -13,14 +13,25 @@ typedef boost::adjacency_list<boost::vecS,
                               boost::vecS,
                               boost::directedS> Arb;
 typedef boost::graph_traits<Arb>::vertex_descriptor Vertex;
+typedef typename boost::graph_traits<Arb>::vertices_size_type vtx_size_type; // num of vertices type
 
 /* Students must adapt this class for their needs; the innards of
    these objects are only accessed by the student's code */
 class HeadStart {
 public:
-  HeadStart(int info) : super_useful_info(info) {}
+  HeadStart(std::vector< vtx_size_type > a, std::vector< vtx_size_type > b) {
+    discover_order = a;
+    finish_order = b;
+  }
+  std::vector< vtx_size_type > getDiscover() const {
+    return discover_order;
+  }
+  std::vector< vtx_size_type > getFinish() const {
+    return finish_order;
+  }
 private:
-  int super_useful_info;
+  std::vector< vtx_size_type > discover_order;
+  std::vector< vtx_size_type > finish_order;
 };
 
 #endif // #ifndef ARB_H
--- ep0/asgt.cpp
+++ ep0/asgt.cpp
@@ -7,9 +7,36 @@
 #include "asgt.h"
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/pending/indirect_cmp.hpp>
 
 using namespace boost;
 using namespace std;
+
+template < typename TimeMap >
+class dfs_time_visitor : public default_dfs_visitor
+{
+    typedef typename property_traits< TimeMap >::value_type T;
+
+public:
+    dfs_time_visitor(TimeMap dmap, TimeMap fmap, T& t)
+    : m_dtimemap(dmap), m_ftimemap(fmap), m_time(t)
+    {
+    }
+    template < typename Vertex, typename Graph >
+    void discover_vertex(Vertex u, const Graph& g) const
+    {
+        put(m_dtimemap, u, m_time++);
+    }
+    template < typename Vertex, typename Graph >
+    void finish_vertex(Vertex u, const Graph& g) const
+    {
+        put(m_ftimemap, u, m_time++);
+    }
+    TimeMap m_dtimemap;
+    TimeMap m_ftimemap;
+    T& m_time;
+};
 
 Arb read_arb(std::istream& in)
 {
@@ -30,7 +57,7 @@ Arb read_arb(std::istream& in)
           arcs.end(),   // EdgeIterator = pair<int, int>
           num_vtx);     // vtx_size_type
 
-  /* DEBUG
+  /* DEBUG 
   cout << "num_vtx: " << num_vtx << endl;
 
   cout << "arcs: " << arcs.size() << endl;
@@ -54,10 +81,60 @@ Arb read_arb(std::istream& in)
 
 HeadStart preprocess(Arb &arb, const Vertex& root)
 {
-  return HeadStart(42);
+  std::vector< vtx_size_type > dtime(num_vertices(arb));
+  std::vector< vtx_size_type > ftime(num_vertices(arb));
+
+  typedef iterator_property_map< std::vector< vtx_size_type >::iterator,
+        property_map< Arb, vertex_index_t >::const_type >
+        time_pm_type;
+  time_pm_type dtime_pm(dtime.begin(), get(vertex_index, arb));
+  time_pm_type ftime_pm(ftime.begin(), get(vertex_index, arb));
+  vtx_size_type t = 0;
+  dfs_time_visitor< time_pm_type > vis(dtime_pm, ftime_pm, t);
+  depth_first_search(arb, visitor(vis));
+
+  
+  std::vector< vtx_size_type > discover_order(num_vertices(arb));
+  integer_range< vtx_size_type > r(0, num_vertices(arb));
+  std::copy(r.begin(), r.end(), discover_order.begin());
+  std::sort(discover_order.begin(), discover_order.end(),
+      indirect_cmp< time_pm_type, std::less< vtx_size_type > >(dtime_pm));
+  /*
+  std::cout << "order of discovery: ";
+  int i;
+  for (i = 0; i < num_vertices(arb); ++i)
+      std::cout << discover_order[i]+1 << " ";
+  */
+  std::vector< vtx_size_type > finish_order(num_vertices(arb));
+  std::copy(r.begin(), r.end(), finish_order.begin());
+  std::sort(finish_order.begin(), finish_order.end(),
+      indirect_cmp< time_pm_type, std::less< vtx_size_type > >(ftime_pm));
+  /*
+  std::cout << std::endl << "order of finish: ";
+  for (i = 0; i < num_vertices(arb); ++i)
+      std::cout << finish_order[i]+1 << " ";
+  std::cout << std::endl;
+  */
+
+  return HeadStart(discover_order, finish_order);
 }
 
 bool is_ancestor (const Vertex& u, const Vertex& v, const HeadStart& data)
 {
-  return false;
+  //cout << data.getDiscover().size() << endl;
+  //cout << data.getFinish().size() << endl;
+  if(u == v) return true;
+  std::vector< vtx_size_type > discover = data.getDiscover();
+  std::vector< vtx_size_type > finish = data.getFinish();
+  int i;
+  for (i = 0; i < discover.size(); i++) {
+    if(discover[i] == u) break;
+    if(discover[i] == v) return false;
+  }
+  for (i = 0; i < finish.size(); i++) {
+    if(finish[i] == u) return false;
+    if(finish[i] == v) break;
+  }
+
+  return true;
 }
