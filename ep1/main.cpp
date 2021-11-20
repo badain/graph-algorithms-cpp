369f7f69b266d60d9c831be278144f1e81a698de
Rafael Badain <badain@usp.br>
2021-11-20 01:59:38 -0300

    ep1 legacy

--- ep1/main.cpp
+++ ep1/main.cpp
@@ -11,11 +11,13 @@ using namespace std;
 // BOOST
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/transpose_graph.hpp>
 using namespace boost;
 
 int debug;
 
 typedef adjacency_list<vecS, vecS, directedS> Digraph;
+typedef graph_traits<Digraph>::vertex_descriptor Vertex;
 int num_variables;
 
 /* Helper Function */
@@ -31,7 +33,89 @@ int neg(int a) {
   return (a + num_variables);
 }
 
-/* Digraph Construction */
+/* Debug Functions */
+/* functions to help development */
+void print_graph(Digraph &digraph) {
+  for_each(vertices(digraph).first, vertices(digraph).second, [&](const auto& vtx) {
+    cout << "Arcs that leave " << convert(vtx) << " go to: ";
+
+    for_each(out_edges(vtx, digraph).first, out_edges(vtx, digraph).second, [&](const auto& arc) {
+        cout << " " << convert(target(arc, digraph));
+    });
+
+    cout << endl;
+  });
+}
+vector<Vertex> get_out_neighbours(Digraph &digraph, Vertex vtx) {
+  vector<Vertex> a;
+  cout << "get_out_neighbours " << vtx << endl;
+  for_each(out_edges(vtx, digraph).first, out_edges(vtx, digraph).second, [&](const auto& arc) {
+    a.push_back(target(arc, digraph));
+  });
+  for (int x : a) cout << x << " ";
+  cout << endl;
+  return a;
+}
+
+/* 2. Finding Strong Connected Components via Kosaraju's */
+void assign_scc(Digraph &t_digraph, Vertex vtx, int root, vector<int>* components) {
+  (*components)[vtx] = root; // each vertex is assigned the root of its SCC
+  
+  // Out-neighbour transposed traversal
+  for_each(out_edges(vtx, t_digraph).first, out_edges(vtx, t_digraph).second, [&](const auto& arc) {
+    Vertex adj = target(arc, t_digraph);
+    if((*components)[adj] == 0) assign_scc(t_digraph, adj, root, components);
+  });
+}
+
+void dfs(Digraph &digraph, Vertex vtx, vector<bool>* visited, vector<Vertex>* inverted_topsort) {
+  (*visited)[vtx] = true; // marks vtx as visited
+  // Out-neighbour traversal
+  for_each(out_edges(vtx, digraph).first, out_edges(vtx, digraph).second, [&](const auto& arc) {
+    Vertex adj = target(arc, digraph);
+    if((*visited)[adj] == false) dfs(digraph, adj, visited, inverted_topsort);
+  });
+
+  (*inverted_topsort).push_back(vtx); // push vtx @ inverted_topsort_array
+}
+
+vector<int> scc_search(Digraph &digraph, Digraph &t_digraph) {
+  // Vector Initialization
+  vector<bool> visited(num_vertices(digraph), false);
+  vector<Vertex> inverted_topsort; // topological order inverted
+  vector<int> components(num_vertices(digraph), 0); // SCC are respresented by its root vertex. Each vertex from the digraph is assigned the root of its SCC.
+
+  // First Kosaraju's traversal
+  auto vp = vertices(digraph);
+  for_each(vp.first, vp.second, [&](const auto& vtx) {
+    if(vtx == 0) return;
+    if(visited[vtx] == false) dfs(digraph, vtx, &visited, &inverted_topsort);
+  });
+
+  // Second Kosaraju's traversal
+  int r = 1;
+  for(int i = (inverted_topsort.size() - 1); i >= 0; i--) {
+    if(components[inverted_topsort[i]] == 0) assign_scc(t_digraph, inverted_topsort[i], r, &components);
+    r = r + 1;
+  }
+
+  if(debug == 1) {
+    for (int h : components) cout << h << " ";
+    cout << endl;
+  }
+
+  /* DEBUG 
+  for (int x : visited) cout << x << " ";
+  cout << endl;
+  for (int y : inverted_topsort) cout << convert(y) << " ";
+  cout << endl;
+  for (int z : components) cout << convert(z) << " ";
+  cout << endl;  */
+  
+  return components;
+}
+
+/* 1. Digraph Construction */
 Digraph read_digraph(istream& in){
     
     in >> num_variables; // extracts number of variables = (num_vertices / 2)
@@ -50,18 +134,15 @@ Digraph read_digraph(istream& in){
         if(debug == 2) cout << convert(neg(a)) << " " << convert(b) << endl << convert(neg(b)) << " " << convert(a) << endl;
     }
 
-    /* DEBUG
-    for_each(vertices(digraph).first, vertices(digraph).second, [&](const auto& vtx) {
-        cout << "Arcs that leave " << convert(vtx) << " go to: ";
-
-        for_each(out_edges(vtx, digraph).first, out_edges(vtx, digraph).second, [&](const auto& arc) {
-            cout << " " << convert(target(arc, digraph));
-        });
-
-        cout << endl;
-    });*/
-
     return digraph;
+}
+
+bool validate(vector<int>* components, vector<bool>* solution) {
+  for(int i = 1; i <= num_variables; i++) {
+    if((*components)[i] == (*components)[i + num_variables]) return false;
+    (*solution)[i] = (*components)[i] > (*components)[i + num_variables];
+  }
+  return true;
 }
 
 /* 2-SAT Solution */
@@ -72,7 +153,29 @@ int main(void)
   
   // Digraph Construction
   Digraph phi = read_digraph(cin);
-  // cout << num_vertices(phi) << " " << num_edges(phi) << endl; // DEBUG
+  Digraph t_phi;
+  transpose_graph(phi, t_phi);
+
+  // SCC Search
+  vector<int> components = scc_search(phi, t_phi);
+  if(debug == 0) {
+    vector<bool> solution(num_variables, false);
+    bool validation = validate(&components, &solution);
+    if(!validation) cout << "NO" << endl;
+    else {
+      cout << "YES ";
+      for (int z : solution) cout << z << " ";
+      cout << endl;
+    }
+  }
+  
+  /* DEBUG
+  cout << "Digraph Phi" << endl;
+  print_graph(phi);
+  cout << num_vertices(phi) << " " << num_edges(phi) << endl;
+  cout << "Digraph Phi Transposed" << endl;
+  print_graph(t_phi);
+   */
 
   return EXIT_SUCCESS;
 }
